730c1e2cce00f5b820bbbb673e449115e290ee6e
Pacharatnkp <pachara.pplzz@gmail.com>
2023-11-29 19:15:46 +0700

    feat:add-answer-ex

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,24 @@
 // Start coding here
+const add = function(a, b) {
+    return a + b;
+};
+const resultAdd = add(10, 5);
+console.log(resultAdd);
+
+const subtract = function(a, b) {
+    return a - b;
+};
+const resultSubtract = subtract(10, 5);
+console.log(resultSubtract);
+
+const multiply = function(a, b) {
+    return a * b;
+};
+const resultMultiply = multiply(10, 5);
+console.log(resultMultiply);
+
+const divide = function(a, b) {
+    return a / b;
+};
+const resultDivide = divide(10, 5);
+console.log(resultDivide);
